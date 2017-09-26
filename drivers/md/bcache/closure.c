494f94aa247d4f2f66287f49c57a51a7b17af70a
Michael Lyle <mlyle@lyle.org>
2017-09-25 21:28:06 -0700

    Revert "bcache: Don't reinvent the wheel but use existing llist API"
    This reverts commit 09b3efec81def807fb225359e34a8e72866dd9c4.

--- drivers/md/bcache/closure.c
+++ drivers/md/bcache/closure.c
@@ -70,10 +70,21 @@ void __closure_wake_up(struct closure_waitlist *wait_list)
 	list = llist_del_all(&wait_list->list);
 
 	/* We first reverse the list to preserve FIFO ordering and fairness */
-	reverse = llist_reverse_order(list);
+
+	while (list) {
+		struct llist_node *t = list;
+		list = llist_next(list);
+
+		t->next = reverse;
+		reverse = t;
+	}
 
 	/* Then do the wakeups */
-	llist_for_each_entry(cl, reverse, list) {
+
+	while (reverse) {
+		cl = container_of(reverse, struct closure, list);
+		reverse = llist_next(reverse);
+
 		closure_set_waiting(cl, 0);
 		closure_sub(cl, CLOSURE_WAITING + 1);
 	}
